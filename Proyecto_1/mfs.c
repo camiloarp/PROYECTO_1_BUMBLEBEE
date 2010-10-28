46d90ec6e39b1c3d8e932e65c88f8464038df5db
camiloarp <camilo_arp@hotmail.com>
2010-10-28 10:38:02 -0600

    funcion crear disco con el mapa de bits ya establecido

--- Proyecto_1/mfs.c
+++ Proyecto_1/mfs.c
@@ -8,9 +8,44 @@ typedef struct
 	int cant_FS;
 	int cant_TS;
 	int tam_mapabits;
+	int inicio_data;
 
 }header;
 
+void marcarbit(int cual,int valor_bit,unsigned char *m){
+
+
+      
+ 
+     if (valor_bit==1)
+       m[cual] = m[cual] | (1 << (7-cual%8));
+     else
+       m[cual] = m[cual] & ~(1 << (7-cual%8));
+         
+    
+}
+
+
+void mostrarbits(unsigned char byte)
+{
+   unsigned char MASK = 1 << 7; 
+
+
+       printf("%c", byte & MASK ? '1' : '0' );
+       byte <<= 1; 
+      
+   
+}
+
+void mostrarmapa(unsigned char *m, int cuantos)
+{
+   int i;
+   for (i=0; i< cuantos; i++)
+   {
+     mostrarbits(m[i]);      
+   }
+   printf("\n ------- fin del mapa --------\n");
+}
 
 void main(int argc, char*argv[]){
 FILE *disco;
@@ -32,7 +67,7 @@ header tempH;
 		}
 		char *block;
 		block = (char *)malloc(sizeof(char)*1024);
-		memset(block,0,1024);
+		memset(block,'0',1024);
 		int i;
 		int  cant_block;
 		cant_block = atoi(argv[2]);
@@ -46,8 +81,16 @@ header tempH;
 		tempH.cant_FS= atoi(argv[3]);
 		tempH.cant_TS= atoi(argv[4]);
 		tempH.tam_mapabits = (cant_block/8)/1024;
-		fseek(disco,0,0);
-		fwrite(&tempH,1024,1,disco);
+		tempH.inicio_data=tempH.tam_mapabits+tempH.cant_FS+tempH.cant_TS+1;
+		fseek(disco,0,SEEK_SET);
+		fwrite(&tempH,1024,1,disco);		
+		int d;
+		for(d=0;d<tempH.inicio_data;d++){		
+		block[d]='1';
+		}
+		fseek(disco,1024,SEEK_SET);
+		fwrite(block,1024,1,disco);
+
 		
 				
 
@@ -59,4 +102,7 @@ header tempH;
 
 
 	}
+
+
+
 }
